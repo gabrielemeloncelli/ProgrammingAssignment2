51aae5de30cdc5d31a8be515127a99b0832be01c
Gabriele Meloncelli <gabriele.b.meloncelli@gmail.com>
2018-08-17 15:41:49 +0200

    Fill the body of the functions as required by the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Set of functions that can handle the caching of the inverted (solved) value
+## of a matrix in order to avoid the overhead of repeated calculations
 
-## Write a short comment describing this function
+## Creates a wrapper for a matrix with cached inverted (solved) value
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        s <- NULL
+        set <- function(y) {
+                x <<- y
+                s <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) s <<- solve
+        getsolve <- function() s
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+
+## Gets the cached inverted (solved) value for the matrix or calculates and caches a fresh one
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        s <- x$getsolve()
+        if (!is.null(s)) {
+                message("getting cached data")
+                return(s)
+        }
+        data <- x$get()
+        s <- solve(data, ...)
+        x$setsolve(s)
+        s
 }
